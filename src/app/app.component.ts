f5300d42bbb6b24c0a6826d9867533f02d74e682
nourabdelhakam <nour.moh93@gmail.com>
2020-05-24 00:15:44 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy, Inject } from '@angular/core';
 import { MediaObserver, MediaChange } from '@angular/flex-layout'
 import { Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +12,53 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'marketing-ng';
   mediaSub: Subscription;
   deviceXs: boolean;
-
-  constructor(public mediaObserver: MediaObserver) {}
+  scrolled = false;
+  menuText = '#fff';
+  backGroundOpacity = "rgba(255, 255, 255, 0)";
+  constructor(public mediaObserver: MediaObserver, @Inject(DOCUMENT) document) { }
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
     this.mediaSub = this.mediaObserver.media$.subscribe(
       (result: MediaChange) => {
         console.log(result.mqAlias);
-        this.deviceXs = result.mqAlias === 'xs' ? true : false;   
+        this.deviceXs = result.mqAlias === 'xs' ? true : false;
       }
     );
   }
   ngOnDestroy() {
     this.mediaSub.unsubscribe();
   }
+  scroll = (event: any): void => {
 
-  onWindowScroll(e) {
-    let element = document.querySelector('.navbar');
-    if (window.pageYOffset > element.clientHeight) {
-      element.classList.add('navbar-inverse');
+    const number = event.srcElement.scrollTop;
+    this.backGroundOpacity = "rgba(255, 255, 255,"+ number/100+")";
+
+    if (number >= 100) {
+      this.scrolled = true;
+      this.menuText = '#000';
     } else {
-      element.classList.remove('navbar-inverse');
+      this.scrolled = false;
+      this.menuText = '#fff';
+    }
+  };
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(e) {
+    const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(e);
+
+    if (scrollOffset >= 100) {
+      document.querySelectorAll('.navbar').forEach((c) => {
+        // c.classList.add('text-smaller');
+        c.classList.remove('transparent');
+      });
+    } else {
+      document.querySelectorAll('.navbar').forEach((c) => {
+        c.classList.add('transparent');
+        // c.classList.remove('text-smaller');
+      });
     }
   }
+
+
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +11,9 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  @HostListener('window:scroll', ['$event'])
+  onScroll(e) {
+    const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(e);
+  }
 }
